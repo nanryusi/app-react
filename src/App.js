065a28df5d66324fb5ed49adcb75b1f4db9fc64b
R.ryusi <nanryusi@gmail.com>
2020-05-31 12:19:58 +0900

    리덕스 데이터 스토어 사용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
-import ProductsAndSuppliers from './ProductsAndSuppliers';
+//import ProductsAndSuppliers from './ProductsAndSuppliers';
+import { Provider } from 'react-redux';
+import dataStore from './store';
+import { Selector } from './Selector';
+import { ProductDisplay } from './ProductDisplay';
+import { SupplierDisplay } from './SupplierDisplay';
 
 export default class App extends Component {
   render() {
-    return <ProductsAndSuppliers></ProductsAndSuppliers>;
+    return (
+      <Provider store={dataStore}>
+        <Selector>
+          <ProductDisplay name="products"></ProductDisplay>
+          <SupplierDisplay name="Suppliers"></SupplierDisplay>
+        </Selector>
+      </Provider>
+    );
   }
 }
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+import modelReducer from './modelReducer';
+
+export default createStore(modelReducer);
+
+export {
+  saveProduct,
+  saveSupplier,
+  deleteProduct,
+  deleteSupplier,
+} from './modelActionCreators';
